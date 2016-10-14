36a96f4a564f9a559445060b57d18395cf1159b4
Joe Fitzgerald <jfitzgerald@pivotal.io>
2016-10-14 13:39:14 -0600

    🛀 Cleanup spec-helper.js

--- spec/spec-helper.js
+++ spec/spec-helper.js
@@ -3,49 +3,50 @@
 
 let completionDelay = 100
 
-beforeEach(function () {
+beforeEach(() => {
   spyOn(atom.views, 'readDocument').andCallFake(fn => fn())
   spyOn(atom.views, 'updateDocument').andCallFake(fn => fn())
   atom.config.set('autocomplete-plus.defaultProvider', 'Symbol')
   atom.config.set('autocomplete-plus.minimumWordLength', 1)
   atom.config.set('autocomplete-plus.suggestionListFollows', 'Word')
   atom.config.set('autocomplete-plus.useCoreMovementCommands', true)
-  return atom.config.set('autocomplete-plus.includeCompletionsFromAllBuffers', false)
+  atom.config.set('autocomplete-plus.includeCompletionsFromAllBuffers', false)
 })
 
-export function triggerAutocompletion (editor, moveCursor = true, char = 'f') {
+let triggerAutocompletion = (editor, moveCursor = true, char = 'f') => {
   if (moveCursor) {
     editor.moveToBottom()
     editor.moveToBeginningOfLine()
   }
   editor.insertText(char)
-  return exports.waitForAutocomplete()
+  exports.waitForAutocomplete()
 }
 
-export function waitForAutocomplete () {
+let waitForAutocomplete = () => {
   advanceClock(completionDelay)
-  return waitsFor('autocomplete to show', done =>
-    setImmediate(function () {
+  return waitsFor('autocomplete to show', (done) => {
+    setImmediate(() => {
       advanceClock(10)
-      return setImmediate(function () {
+      setImmediate(() => {
         advanceClock(10)
-        return done()
+        done()
       })
     })
-
-  )
+  })
 }
 
-export function buildIMECompositionEvent (event, {data, target} = {}) {
+let buildIMECompositionEvent = (event, {data, target} = {}) => {
   event = new CustomEvent(event, {bubbles: true})
   event.data = data
   Object.defineProperty(event, 'target', {get () { return target }})
   return event
 }
 
-export function buildTextInputEvent ({data, target}) {
+let buildTextInputEvent = ({data, target}) => {
   let event = new CustomEvent('textInput', {bubbles: true})
   event.data = data
   Object.defineProperty(event, 'target', {get () { return target }})
   return event
 }
+
+export {triggerAutocompletion, waitForAutocomplete, buildIMECompositionEvent, buildTextInputEvent}

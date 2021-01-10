95c73101ab3e698f10793b1e6cc4282061016252
Lev Chelyadinov <leva181777@gmail.com>
2021-01-10 22:49:37 +0300

    Update spec/custom-runner.js
    Co-authored-by: Amin Yahyaabadi <aminyahyaabadi74@gmail.com>

--- spec/custom-runner.js
+++ spec/custom-runner.js
@@ -1,21 +1,7 @@
 const { createRunner } = require('atom-jasmine3-test-runner')
 
 const options = {
-  specHelper: {
-    atom: true,
-    attachToDom: true,
-    ci: true,
-    customMatchers: true,
-    jasmineFocused: true,
-    jasmineJson: true,
-    jasminePass: true,
-    jasmineTagged: true,
-    mockClock: true,
-    mockLocalStorage: true,
-    profile: true,
-    set: true,
-    unspy: true
-  },
+  specHelper: true,
   silentInstallation: true
 }
 
